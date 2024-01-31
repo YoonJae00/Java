b2441200985b7eee2f49a53282e189995cd43d89
yoonjae <realyoon77@gmail.com>
2024-01-31 17:30:33 +0900

    [수업내용 정리] 2024-01-31

---
+++ src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        /*수업목표. 여러 가지의 값의 형태를 출력할 수 있다.*/
+
+        /*목차. 1. 숫자 형태의 값*/
+
+        System.out.println(123);
+
+        /*목차. 1-1. 실수 형태의 값 출력*/
+
+        System.out.println(1.24);
+
+        /*목차. 1-2. 문자 형태의 값 출력*/
+
+        System.out.println('a');            // 문자 형태의 값은 홀따옴표(single-quotation)으로 감싸주어야 한다.
+//        System.out.println('ab');         // 두 개 이상은 문자로 취급하지 않기 때문에 에러가 발생한다.
+//        System.out.println('');           // 아무 문자도 기록되지 않는 경우도 에러가 발생한다.
+        System.out.println('1');            // 숫자 값 이지만 홀따옴표로 감싸져 있는 경우는 문자 '1' 이라고 판단한다.
+
+        /*목차.1-3. 문자열 형태의 값 출력*/
+        System.out.println("안녕하세요 김윤재 입니다.");// 문자열은 문자 여러 개가 나열된 형태를 말하며 쌍따옴표(double-quoation)으로 감싸주어야한다
+        System.out.println("123");                   // 정수이지만 쌍따옴표로 감싸져있기 때문에 문자열로 보아야 한다.
+        System.out.println("");                      // 아무 값도 없는 빈 쌍따옴표도 문자열로 취급한다.
+        System.out.println("a");                     // 한 개의 문자도 쌍따옴표로 감싸면 문자열이다. ( 문자 a와는 다르다.)
+
+        /*목차.1-4. 논리 형태의 값 출력*/
+        System.out.println(true);
+        System.out.println(false);          //true 혹은 false 값을 논리형이라고 한다.
+
+
+    }
+}
---
+++ src/main/java/com/ohgiraffers/section01/literal/Application03.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application03 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. 문자열 합치기 결과를 예측하고 사용할 수 있다.*/
+        /* 목차. 1. 두 개의 문자열 합치기 */
+        System.out.println("================ 두 개의 문자열 합치기 ================");
+        System.out.println(9 + 9);      //값 = 9
+        System.out.println("9" + 9);    //값 = 99
+        System.out.println(9 + "9");    //값 = 99
+        System.out.println("9" + "9");  //값 = 99
+
+        /* 목차. 2. 세 개의 문자열 합치기 */
+        System.out.println("================ 두 개의 문자열 합치기 ================");
+        System.out.println(9 + 9 + "9");    // 일반적인 연산 방향 ======>
+        System.out.println(9 + "9" + 9);
+        System.out.println("9" + 9 + 9);    // 연산 방향 대로 가면 문자열로 합쳐진다.
+        /* System.out.println("9" + 9 + 9);    // Q. 이 결과를 918 로 만들고 싶다.*/
+        System.out.println("9" + (9 + 9));
+
+        System.out.println("============ 10과 20의 사칙연산 결과 보기 좋게 출력하기 ============");
+        System.out.println("10과 20의 합 : " + (10 + 20));     /* 괄호를 묶지 안으면 문자열 합치기가
+                                                                 일어나서 결과가 1020이 된다.*/
+        System.out.println("10과 20의 합 : " + 10 + 20);       // why? 문자열이 연산방향 때문에 붙어진다.
+
+
+
+
+    }
+}
